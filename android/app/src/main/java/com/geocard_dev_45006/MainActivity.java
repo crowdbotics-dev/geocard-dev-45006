63eeab985a73f6bb9ce44341c6fdf90232b3985f
Crowdbotics <engineering@crowdbotics.com>
2022-11-17 06:44:29 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/geocard_dev_45006/MainActivity.java
@@ -0,0 +1,15 @@
+package com.geocard_dev_45006;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "geocard_dev_45006";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "geocard-dev-45006",
+  url: "https://geocard-dev-45006.botics.co",
+  api: "https://geocard-dev-45006.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
